484c6fa2d8b42d11e7b3553426a81dc9ac365f37
Pablo Luis Reinoso Santana <pabloreinoso0@gmail.com>
2018-01-05 16:10:58 -0400

    service worker added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatToolbarModule, MatCardModule} from '@angular/material';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
   imports: [
     BrowserModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
